67846373cd983dae67f8466baf7932eed83f41e3
KarenWest <KarenWest15@gmail.com>
2013-11-14 13:57:33 -0500

    hw2 saas1 files

---
+++ hw2help.rb
@@ -0,0 +1,12 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, first visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+
+class Movie < ActiveRecord::Base
+
+
+ def self.all_ratings
+  ratings = Movie.select(:rating).map(&:rating).uniq
+ end
+
+end
